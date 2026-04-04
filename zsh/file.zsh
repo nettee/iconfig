26e2d2e808f83362c50127aa1427ffa43a41610c
nettee <nettee.liu@gmail.com>
2026-04-04 19:42:26 +0800

    als

--- zsh/file.zsh
+++ zsh/file.zsh
@@ -1,11 +1,16 @@
 # 获取文件/目录的绝对路径
 als() {
+  local abs_path
+
   if [ $# -eq 0 ]; then
     # 如果没有参数，输出当前目录
-    pwd
+    abs_path="$(pwd)"
   else
     # 如果有参数，输出参数的绝对路径
     # :a 是 ZSH 特有的修饰符，用于获取绝对路径
-    echo "${1:a}"
+    abs_path="${1:a}"
   fi
+
+  print -r -- "$abs_path"
+  print -rn -- "$abs_path" | /usr/bin/pbcopy
 }
