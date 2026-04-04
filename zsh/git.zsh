866399710ae90b40a5ef15fe38510f5fdad9d7aa
William <nettee.liu@gmail.com>
2026-04-04 20:32:37 +0800

    git config

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,4 +1,5 @@
 alias groot='git rev-parse --show-toplevel'
 alias gbr='git branch --show-current | tr -d "\n"'
+alias gls='git --no-pager log --pretty=format:"%C(green)%ad %C(cyan)%<(8,trunc)%an %C(yellow)%h%C(auto)%d %C(reset)%s" --date=format:"%Y-%m-%d %H:%M:%S" -n 10'
 
 alias lg='lazygit'
