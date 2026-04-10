7f412b158bffba32c0604ed5d3f9b75b883a78ec
nettee <nettee.liu@gmail.com>
2026-04-10 22:51:38 +0800

    gitconfig

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,4 @@
+alias g='git'
 alias groot='git rev-parse --show-toplevel'
 alias gbr='git branch --show-current | tr -d "\n"'
 alias gls='git --no-pager log --pretty=format:"%C(green)%ad %C(cyan)%<(8,trunc)%an %C(yellow)%h%C(auto)%d %C(reset)%s" --date=format:"%Y-%m-%d %H:%M:%S" -n 10'
