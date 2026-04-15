cc903c9c7087eafdadb4322aab368abfb359a105
nettee <nettee.liu@gmail.com>
2026-04-15 19:57:50 +0800

    gbr

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,6 +1,6 @@
 alias g='git'
 alias groot='git rev-parse --show-toplevel'
-alias gbr='git branch --show-current | tr -d "\n"'
+alias gbr='git branch --show-current | tr -d "\n" | tee >(pbcopy)'
 alias gls='git --no-pager log --pretty=format:"%C(green)%ad %C(cyan)%<(8,trunc)%an %C(yellow)%h%C(auto)%d %C(reset)%s" --date=format:"%Y-%m-%d %H:%M:%S" -n 10'
 
 alias lg='lazygit'
