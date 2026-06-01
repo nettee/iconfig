9b143e68ae507207513ca11b9def4ed08dc66b14
nettee <nettee.liu@gmail.com>
2026-06-01 14:48:18 +0800

    rls

--- zsh/file.zsh
+++ zsh/file.zsh
@@ -14,3 +14,24 @@ als() {
   print -r -- "$abs_path"
   print -rn -- "$abs_path" | /usr/bin/pbcopy
 }
+
+# 获取绝对路径相对于当前目录的相对路径
+rls() {
+  local abs_path rel_path
+
+  if [ $# -eq 0 ]; then
+    print -u2 -- "usage: rls <absolute-path>"
+    return 1
+  fi
+
+  abs_path="$1"
+  if [[ "$abs_path" != /* ]]; then
+    print -u2 -- "rls: expected absolute path: $abs_path"
+    return 1
+  fi
+
+  rel_path="$(python3 -c 'import os, sys; print(os.path.relpath(sys.argv[1], os.getcwd()))' "$abs_path")" || return
+
+  print -r -- "$rel_path"
+  print -rn -- "$rel_path" | /usr/bin/pbcopy
+}
