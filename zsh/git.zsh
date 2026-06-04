e48044395f3d016e4dde34753add436b7b480c1f
nettee <nettee.liu@gmail.com>
2026-06-04 11:26:03 +0800

    gmp

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -2,5 +2,6 @@ alias g='git'
 alias groot='git rev-parse --show-toplevel'
 alias gbr='git branch --show-current | tr -d "\n" | tee >(pbcopy)'
 alias gls='git --no-pager log --pretty=format:"%C(green)%ad %C(cyan)%<(8,trunc)%an %C(yellow)%h%C(auto)%d %C(reset)%s" --date=format:"%Y-%m-%d %H:%M:%S" -n 10'
+alias gmp='git checkout main && git pull'
 
 alias lg='lazygit'
